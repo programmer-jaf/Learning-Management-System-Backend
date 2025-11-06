a83080b41b164c63c2311dc18d08c2633ed0895a
Programmer-Jaf <programmerjaf@gmail.com>
2025-11-06 19:10:43 +0600

    feat(interface): create purchase interface

---
+++ src/interfaces/purchase.interfaces.ts
@@ -0,0 +1,11 @@
+import { Document, Types } from 'mongoose';
+
+export interface IPurchase extends Document {
+  user: Types.ObjectId;
+  course: Types.ObjectId;
+  price: number;
+  paymentMethod: 'credit' | 'debit' | 'stripe';
+  transactionId: string;
+  status: 'success' | 'failed' | 'pending';
+  createdAt: Date;
+}
